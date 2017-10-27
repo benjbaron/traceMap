83e2240b5edb7fc6300868de2535ef8547bfb762
Benjamin Baron <benjamin.baron@me.com>
2017-10-27 16:06:03 +0100

    fixed bug

--- app.py
+++ app.py
@@ -74,7 +74,7 @@ def process_trace():
 	return json.dumps({"trace_id": trace_id, "user_id": user_id})
 
 
-@app.upload('/upload', method = ['POST'])
+@app.route('/upload', method = ['POST'])
 def upload():
 	data = request.json
 	print(data)
